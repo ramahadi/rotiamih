2f94d241928cec8fa40dec2c5d87882243d6b24a
ramahadi <ramahadi.satria@gmail.com>
2025-04-13 10:30:29 +0700

    part 29

--- ui/header.php
+++ ui/header.php
@@ -198,68 +198,140 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <!-- <li class="nav-item menu-open">
-            <a href="#" class="nav-link active">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
+               <li class="nav-item">
+            <a href="dashboard.php" class="nav-link">
+            <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
-                Starter Pages
-                <i class="right fas fa-angle-left"></i>
+                Dashboard
+                
+              </p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="category.php" class="nav-link">
+            <i class="nav-icon fas fa-table"></i>
+            <p>
+                Category
+              
+              </p>
+            </a>
+          </li>
+
+
+          <li class="nav-item">
+            <a href="addproduct.php" class="nav-link">
+            <i class="nav-icon fas fa-edit"></i>
+              <p>
+               Product
+              
+              </p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="productlist.php" class="nav-link">
+            <i class="nav-icon fas fa-list"></i>
+              <p>
+               Product List
+              
+              </p>
+            </a>
+          </li>
+
+
+
+
+
+          <li class="nav-item">
+            <a href="pos.php" class="nav-link">
+            <i class="nav-icon fas fa-book"></i>
+              <p>
+               POS
+              
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="orderlist.php" class="nav-link">
+            <i class="nav-icon fas fa-list"></i>
+              <p>
+             OrderList
+              
+              </p>
+            </a>
+          </li>
+
+          
+
+
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-chart-pie"></i>
+              <p>
+                Sales Report
+                <i class="fas fa-angle-left right"></i>
               </p>
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="#" class="nav-link active">
+                <a href="tablereport.php" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Active Page</p>
+                  <p>Table Report</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="#" class="nav-link">
+                <a href="graphreport.php" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Inactive Page</p>
+                  <p>Graph Report</p>
                 </a>
               </li>
             </ul>
-          </li> -->
+          </li>
+
+      
+
           <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
+            <a href="taxdis.php" class="nav-link">
+            <i class="nav-icon fas fa-calculator"></i>
               <p>
-                Dashboard
-                <!-- <span class="right badge badge-danger">New</span> -->
+               Tax-(SGST-CGST)
+              
               </p>
             </a>
           </li>
 
           <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
+            <a href="registration.php" class="nav-link">
+            <i class="nav-icon far fa-plus-square"></i>
               <p>
-                Category
-                <!-- <span class="right badge badge-danger">New</span> -->
+               Registration
+              
               </p>
             </a>
           </li>
 
           <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
+            <a href="changepassword.php" class="nav-link">
+            <i class="nav-icon fas fa-user-lock"></i>
               <p>
-                Registration
-                <!-- <span class="right badge badge-danger">New</span> -->
+               Change Password
+              
               </p>
             </a>
           </li>
+
 
           <li class="nav-item">
             <a href="logout.php" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
+            <i class="nav-icon fas fa-sign-out-alt"></i>
               <p>
                 Logout
-                <!-- <span class="right badge badge-danger">New</span> -->
+               
               </p>
             </a>
           </li>
+
 
         </ul>
       </nav>
