d6890667820bed8edebd9d494b236aec089baa16
ramahadi satria <ramahadi.satria@gmail.com>
2025-05-11 19:48:58 +0700

    delete product final

---
+++ ui/productdelete.php
@@ -0,0 +1,21 @@
+<?php
+include_once'connectdb.php';
+
+
+
+$id=$_POST['pidd'];
+$sql="delete from tbl_product where pid =$id";
+
+$delete=$pdo->prepare($sql);
+
+if($delete->execute()){
+
+}else{
+
+    echo"Error in deleting product";
+}
+
+
+
+
+?>
